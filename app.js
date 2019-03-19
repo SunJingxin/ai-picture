3b90335c688e9165a8cf9b6093db29c68e7f5c6a
sunjx <1825951386@qq.com>
2019-03-19 16:22:03 +0800

    -M端口号

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!2019'))
+app.get('/', (req, res) => res.send('Hello World!2019~'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
